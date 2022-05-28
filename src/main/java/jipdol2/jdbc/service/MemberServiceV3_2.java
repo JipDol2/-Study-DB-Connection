bcb7ad876aa931ef7f0440675aa919ff64c7ad1d
seongju lee <gch03915@naver.com>
2022-05-28 16:08:31 +0900

    complete Version 3.2

---
+++ src/main/java/jipdol2/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,76 @@
+package jipdol2.jdbc.service;
+
+import jipdol2.jdbc.domain.Member;
+import jipdol2.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+//    private final DataSource dataSource;
+//    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 repository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 repository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.repository = repository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        txTemplate.executeWithoutResult((status)->{
+            /**
+             *  트랜잭션 템플릿을 사용했음에도 불구하고 완벽하게 비지니스 로직만 남기지는 못했다...
+             *  이를 해결하기 위해 AOP를 사용한다!!!
+             */
+            try {
+                bizLogic(fromId,toId,money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = repository.findById(fromId);
+        Member toMember = repository.findById(toId);
+
+        repository.update(fromId,fromMember.getMoney()- money);
+        validation(toMember);
+        repository.update(toId,toMember.getMoney()+ money);
+    }
+
+    private void release(Connection con) {
+        if(con !=null){
+            try{
+                /**
+                 * connection을 close 하면 종료시키는 것이 아니라 connection pool 에 반납하게 된다.
+                 * 그래서 이 connection을 다시 사용하는 사람은 자동커밋이 되어있다고 생각한다.
+                 * 그렇기 때문에 자동커밋으로 설정해주어야 한다.
+                 */
+                con.setAutoCommit(true);
+                con.close();
+            }catch (Exception e){
+                log.info("error",e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
